8bb1e533b78c2aa6764416c2eee2a5f9053b24b6
Bilva Prasad <47714144+Bilva97@users.noreply.github.com>
2023-04-28 13:20:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
  public static void main(String args[])
  {
-   System.out.println("Hello,Right now learning jenkins and deploying the dockers");
+   System.out.println("Hello,Right now learning jenkins and deploying the dockers and learning the things");
    }
  }  
